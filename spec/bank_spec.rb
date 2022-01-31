67bbdea65e1e56b6e39ff7cd92f61374ec5b6b79
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-31 12:32:49 +0000

    Test written for withdraw  made with a given date passed as an argument. passing

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -72,5 +72,11 @@ describe Bank do
       expect(@bank.date.month).to equal Date.today.month
       expect(@bank.date.year).to equal Date.today.year
     end
+    it('has an associated date') do
+      @bank.withdraw(100, Date.new(2021, 07, 06))
+      expect(@bank.date.day).to equal 6
+      expect(@bank.date.month).to equal 7
+      expect(@bank.date.year).to equal 2021
+    end
   end
 end
