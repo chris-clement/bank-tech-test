255f4c938a82737f4b7578604fc648439c28aa2d
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-01 10:50:03 +0000

    All tests moved from bank_spec to bank_account_display_spec now passing by providing the history

--- spec/bank_account_display_spec.rb
+++ spec/bank_account_display_spec.rb
@@ -43,19 +43,23 @@ describe BankAccountDisplay do
         @bank_account_display.print_summary(history)
       end.to output("date || credit || debit || balance\n 31/12/2021 || || 50.00 || 50.00\n 30/12/2021 || 100.00 || || 100.00\n").to_stdout
     end
-    # it('prints the transaction summary of a multiple transactions in order of newest to oldest') do
-    #   deposit_date = Date.new(2023, 1, 10)
-    #   @bank.deposit(1000, deposit_date)
+    it('prints the transaction summary of a multiple transactions in order of newest to oldest') do
+      deposit_date = Date.new(2023, 1, 10)
+      # @bank.deposit(1000, deposit_date)
 
-    #   deposit_date2 = Date.new(2023, 1, 13)
-    #   @bank.deposit(2000, deposit_date2)
+      deposit_date2 = Date.new(2023, 1, 13)
+      # @bank.deposit(2000, deposit_date2)
 
-    #   withdraw_date = Date.new(2023, 1, 14)
-    #   @bank.withdraw(500, withdraw_date)
+      withdraw_date = Date.new(2023, 1, 14)
+      # @bank.withdraw(500, withdraw_date)
 
-    #   expect do
-    #     @bank_account_display.print_summary
-    #   end.to output("date || credit || debit || balance\n 14/01/2023 || || 500.00 || 2500.00\n 13/01/2023 || 2000.00 || || 3000.00\n 10/01/2023 || 1000.00 || || 1000.00\n").to_stdout
-    # end
+      history = [{ date: deposit_date, credit: 1000, debit: 0, balance: 1000 }, 
+                 { date: deposit_date2, credit: 2000, debit: 0, balance: 3000 },
+                 { date: withdraw_date, credit: 0, debit: 500, balance: 2500 }
+                ]
+      expect do
+        @bank_account_display.print_summary(history)
+      end.to output("date || credit || debit || balance\n 14/01/2023 || || 500.00 || 2500.00\n 13/01/2023 || 2000.00 || || 3000.00\n 10/01/2023 || 1000.00 || || 1000.00\n").to_stdout
+    end
   end
 end
