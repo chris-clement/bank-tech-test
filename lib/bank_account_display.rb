320329802806aa7d826bfdaf948b774475a82ee1
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-01 14:27:30 +0000

    print_summary puts statements formatted over multiple lines for clarity

--- lib/bank_account_display.rb
+++ lib/bank_account_display.rb
@@ -4,12 +4,23 @@ class BankAccountDisplay
 
   def print_summary(history)
     sort_transaction_history(history)
-    puts "date".center(COLUMN_SPACING) + "||" + "credit".center(COLUMN_SPACING) + "||" + "debit".center(COLUMN_SPACING) + "||" + "balance".center(COLUMN_SPACING)
+
+    puts "date".center(COLUMN_SPACING) + "||" + 
+    "credit".center(COLUMN_SPACING) + "||" + 
+    "debit".center(COLUMN_SPACING) + "||" + 
+    "balance".center(COLUMN_SPACING)
+
     history.each do |transaction|
       if debit_transaction?(transaction)
-        puts "#{transaction[:date].strftime('%d/%m/%Y')}".center(COLUMN_SPACING) + "||" + "".center(COLUMN_SPACING) + "||" + "#{sprintf('%.2f', transaction[:debit])}".center(COLUMN_SPACING) + "||" + "#{sprintf('%.2f', transaction[:balance])}".center(COLUMN_SPACING)
+        puts "#{transaction[:date].strftime('%d/%m/%Y')}".center(COLUMN_SPACING) + "||" + 
+        "".center(COLUMN_SPACING) + "||" + 
+        "#{sprintf('%.2f', transaction[:debit])}".center(COLUMN_SPACING) + "||" + 
+        "#{sprintf('%.2f', transaction[:balance])}".center(COLUMN_SPACING)
       else
-        puts " #{transaction[:date].strftime('%d/%m/%Y')}".center(COLUMN_SPACING) + "||" + "#{sprintf('%.2f', transaction[:credit])}".center(COLUMN_SPACING) + "||" + "".center(COLUMN_SPACING) + "||" + "#{sprintf('%.2f', transaction[:balance])}".center(COLUMN_SPACING)
+        puts "#{transaction[:date].strftime('%d/%m/%Y')}".center(COLUMN_SPACING) + "||" + 
+        "#{sprintf('%.2f', transaction[:credit])}".center(COLUMN_SPACING) + "||" + 
+        "".center(COLUMN_SPACING) + "||" + 
+        "#{sprintf('%.2f', transaction[:balance])}".center(COLUMN_SPACING)
       end
     end
   end
